5836b35b762f5e1315c56416d731c9ce675dc724
LAustinSauce <155275978+BartonBase@users.noreply.github.com>
2025-03-05 17:50:38 -0700

    Update broth-invaders.js

--- games/broth-invaders.js
+++ games/broth-invaders.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-
+canvas.width = 800;
+canvas.height = 600;
 // Constants for readability
 const BASE_INVADER_SHOOT_INTERVAL = 264;
 const ENEMY_SPAWN_DELAY = 240;
